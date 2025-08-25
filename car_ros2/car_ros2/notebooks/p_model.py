01c14ae5daeb543ce13e2508313eb807d4d3187a
Akshat-Shu <shuklaakshat.217@gmail.com>
2025-08-25 23:00:58 +0530

    Create PModel.py

---
+++ car_ros2/car_ros2/notebooks/p_model.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+class PModel(nn.Module):
+    def __init__(self, dims=[39, 128, 128, 64, 1]):
+        super(PModel, self).__init__()
+        self.seq = nn.Sequential()
+        self.bn = nn.BatchNorm1d(dims[0], affine=False)
+        
+        
+        for i in range(1, 4):
+            self.seq.append(nn.Linear(dims[i-1], dims[i]))
+            if i < 3: self.seq.append(nn.ReLU())
+
+    def forward(self, x: torch.Tensor):
+        if len(x.shape) == 2:
+            x = self.bn(x)
+        elif len(x.shape) == 3:
+            x1 = self.bn(x.reshape((-1, x.shape[-1])))
+            x = x1.reshape(x.shape)
+        
+        out = self.seq(x)
+        return out
